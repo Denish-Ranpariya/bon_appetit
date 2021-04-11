b21985323153f4e5b7021fc51b815ac0a5ceff52
ManojPadia <padia.manoj@gmail.com>
2021-04-11 18:08:24 +0530

    Added pending order card

--- lib/screens/pending_orders_screen.dart
+++ lib/screens/pending_orders_screen.dart
@@ -1,5 +1,6 @@
 import 'package:bon_appetit/models/Order.dart';
 import 'package:bon_appetit/services/database_service.dart';
+import 'package:bon_appetit/shared/constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -29,103 +30,100 @@ class _PendingOrderScreenState extends State<PendingOrderScreen> {
           if (snapshot.data != null) {
             List<Order> orders = snapshot.data;
             return ListView.builder(
+              padding: EdgeInsets.symmetric(horizontal: 15.0,vertical: 10.0),
               itemCount: orders.length,
               itemBuilder: (context, outerIndex) {
-                // return Text(
-                //   snapshot.data[outer_index].name
-                // );
-                return Column(
-                  children: [
-                    Text(
-                      orders[outerIndex].name,
-                    ),
-                    Text(
-                      orders[outerIndex].orderId,
-                    ),
-                    // StreamBuilder<List<OrderedFoodItem>>(
-                    //   stream: orders[outerIndex].orderedFoodItemList,
-                    //   builder: (context,innerSnapshot){
-                    //     if(innerSnapshot.hasData){
-                    //       List<OrderedFoodItem> foodItemList = innerSnapshot.data;
-                    //       return ListView.builder(
-                    //         shrinkWrap: true,
-                    //         itemCount: foodItemList.length,
-                    //         itemBuilder: (context, innerIndex) {
-                    //           return Column(
-                    //             children: [
-                    //               Text(
-                    //                   foodItemList[innerIndex].foodItemName
-                    //               ),
-                    //               Text(
-                    //                   foodItemList[innerIndex].foodItemPrice
-                    //               ),
-                    //               Text(
-                    //                   foodItemList[innerIndex].foodItemQuantity
-                    //               ),
-                    //             ],
-                    //           );
-                    //         },
-                    //       );
-                    //     } else {
-                    //       return Text('no data');
-                    //     }
-                    //   },
-                    // ),
-                    StreamBuilder(
-                      stream: FirebaseFirestore.instance
-                          .collection(FirebaseAuth.instance.currentUser.uid +
-                              'pendingorders')
-                          .doc(orders[outerIndex].orderId)
-                          .collection('ordered_fooditems_collection')
-                          .snapshots(),
-                      builder: (context, innerSnapshot) {
-                        if (innerSnapshot.hasData) {
-                          // return Text(innerSnapshot.data.docs.map((document){
-                          //   document.get['fooditem_name'].toString();
-                          // }));
-                          return ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: innerSnapshot.data.docs.length,
-                            itemBuilder: (context, index) {
-                              return Column(
-                                children: [
-                                  Text(
-                                    innerSnapshot.data.docs[index]
-                                        .get('fooditem_name')
-                                        .toString(),
-                                    // style: TextStyle(
-                                    //     fontSize: 20,
-                                    //     fontWeight: FontWeight.bold),
-                                  ),
-                                  Text(innerSnapshot.data.docs[index]
-                                      .get('fooditem_price')
-                                      .toString()),
-                                  Text(innerSnapshot.data.docs[index]
-                                      .get('fooditem_quantity')
-                                      .toString()),
-                                ],
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 18.0,vertical: 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Ordered by : "+orders[outerIndex].name,
+                          style: kTileTextStyle,
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Center(
+                          child: Text(
+                            "(Order id : "+orders[outerIndex].orderId+")",
+                            style: kTileTextStyle.copyWith(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                        Divider(
+                          thickness: 2.0,
+                        ),
+                        Text(
+                          "Items List :",
+                          style: kTileTextStyle,
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        StreamBuilder(
+                          stream: FirebaseFirestore.instance
+                              .collection(FirebaseAuth.instance.currentUser.uid +
+                                  'pendingorders')
+                              .doc(orders[outerIndex].orderId)
+                              .collection('ordered_fooditems_collection')
+                              .snapshots(),
+                          builder: (context, innerSnapshot) {
+                            if (innerSnapshot.hasData) {
+                              return ListView.builder(
+                                shrinkWrap: true,
+                                itemCount: innerSnapshot.data.docs.length,
+                                itemBuilder: (context, index) {
+                                  return Row(
+                                    children: [
+                                      Text(
+                                        innerSnapshot.data.docs[index]
+                                            .get('fooditem_name')
+                                            .toString(),
+                                        style: kTileTextStyle.copyWith(
+                                          fontSize: 16.0
+                                        ),
+                                      ),
+                                      Spacer(),
+                                      Text(innerSnapshot.data.docs[index]
+                                          .get('fooditem_quantity')
+                                          .toString()),
+                                      SizedBox(
+                                        width: 5.0,
+                                      ),
+                                      Text("x"),
+                                      SizedBox(
+                                        width: 5.0,
+                                      ),
+                                      Text(innerSnapshot.data.docs[index]
+                                          .get('fooditem_price')
+                                          .toString()),
+
+
+                                    ],
+                                  );
+                                },
                               );
-                            },
-                          );
-                        } else {
-                          return Text('No data');
-                        }
-                      },
+                            } else {
+                              return CircularProgressIndicator();
+                            }
+                          },
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 );
               },
             );
           } else {
-            return Text('no data');
+            return CircularProgressIndicator();
           }
         },
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () async {
-      //     await DatabaseService(uid: FirebaseAuth.instance.currentUser.uid).getPendingOrderCollectionDocuments();
-      //   },
-      // ),
     );
   }
 }
